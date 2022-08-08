c3aa1f45a259f2c7f11aab1b4613d303aa241c55
Andy Pimlott <andy.pimlott17@hotmail.co.uk>
2022-08-08 10:26:05 +0100

    Initial Commit

---
+++ src/main/java/com/qa/todolistproject/QaToDoListSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.qa.todolistproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaToDoListSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaToDoListSpringProjectApplication.class, args);
+	}
+
+}
